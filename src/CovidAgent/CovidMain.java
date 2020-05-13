cf4f2c8b45e373c5cbc7269e7c7db65b17c6a731
martin97peru <martin97_peru@hotmail.com>
2020-05-13 15:26:25 -0300

    Listo para probar (O sea, falta un monton todavia)

--- src/CovidAgent/CovidMain.java
+++ src/CovidAgent/CovidMain.java
@@ -1,5 +1,17 @@
 package CovidAgent;
 
-public class CovidMain {
+import frsf.cidisi.faia.exceptions.PrologConnectorException;
+import frsf.cidisi.faia.simulator.SearchBasedAgentSimulator;
 
+public class CovidMain {
+    public static void main(String[] args) throws PrologConnectorException {
+        CovidAgent covidAgent = new CovidAgent();
+
+        CovidEnvironment covidEnvironment = new CovidEnvironment();
+
+        SearchBasedAgentSimulator simulator =
+                new SearchBasedAgentSimulator(covidEnvironment, covidAgent);
+
+        simulator.start();
+    }
 }
